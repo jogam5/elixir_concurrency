b7b8e8755ab56dd8b6c0afe67966dd70617ab3a1
Jose Gabriel Islas <jogam5@yahoo.com.mx>
2023-12-03 12:09:07 -0500

    aync partially working: too many open files errors

--- sender/lib/crawler.ex
+++ sender/lib/crawler.ex
@@ -9,6 +9,11 @@ defmodule Crawler do
   4. Download the image
   5. Save the image with a given file in a given path
   """
+
+  defmodule TaskError do
+    defstruct [:reason]
+  end
+
   def retrieve_lines_in_file(file_path) do
     case File.read(file_path) do
       {:ok, content} -> 
@@ -30,6 +35,8 @@ defmodule Crawler do
       {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
         File.write!(filename, body)
         IO.puts "Image downloaded #{filename}"
+        :timer.sleep(500)
+        File.close(filename)
         {:ok, body}
       {:ok, %HTTPoison.Response{status_code: status_code}} ->
         IO.puts "Unexpected response: #{status_code} in url: #{url}"
@@ -56,6 +63,88 @@ defmodule Crawler do
       download_single_image(url, filename)
     end)
   end
+  def download_images_async_stream(file_path, output_path) do
+    urls = retrieve_lines_in_file(file_path)
+
+    result_stream =
+      Task.async_stream(Enum.with_index(urls), fn {url, i} ->
+        img_name = "img_#{i + 1}.jpg"
+        filename = Path.join(output_path, img_name)
+
+        try do
+          case download_single_image(url, filename) do
+            {:ok, _binary_data} ->
+              {:ok, "Image downloaded"}
+            {:error, %HTTPoison.Error{reason: :timeout}} ->
+              #IO.puts "Timeout downloading image: #{url}"
+              {:error, :timeout}
+            {:error, reason} ->
+              #IO.puts "Error downloading image: #{reason}"
+              {:error, reason}
+          end
+        catch
+          exception ->
+            case exception do
+              %HTTPoison.Error{reason: :timeout} ->
+                IO.puts "Timeout downloading image: #{url}"
+                {:error, :timeout}
+              _ ->
+                IO.puts "Error downloading image: #{inspect(exception)}"
+                {:error, :unknown}
+            end
+        end
+      end, timeout: 10000, max_concurrency: 2)
+
+    result_stream
+    |> Enum.map(&handle_task_result/1)
+  end
+
+
+  #def download_images_async_stream(file_path, output_path) do
+  #  urls = retrieve_lines_in_file(file_path)
+
+  #  result_stream =
+  #    Task.async_stream(Enum.with_index(urls), fn {url, i} ->
+  #      img_name = "img_#{i + 1}.jpg"
+  #      filename = Path.join(output_path, img_name)
+
+  #      case download_single_image(url, filename) do
+  #        {:ok, _binary_data} ->
+  #          {:ok, "Image downloaded"}
+  #        {:error, %TaskError{reason: :timeout}} ->
+  #          IO.puts "Timeout downloading image: #{url}"
+  #          {:error, :timeout}
+  #        {:error, reason} ->
+  #          IO.puts "Error downloading image: #{reason}"
+  #          {:error, reason}
+  #      end
+  #    end)
+
+  #  handle_results(result_stream)
+  #end
+
+
+
+  defp handle_results(result_stream) do
+    result_stream
+    |> Enum.map(&handle_task_result/1)
+  end
+
+  defp handle_task_result({:ok, {:ok, "Image downloaded"}}) do
+    IO.puts "Image downloaded successfully."
+    {:ok, "Image downloaded"}
+  end
+
+  defp handle_task_result({:ok, {:error, :timeout}}) do
+    IO.puts "Timeout downloading image."
+    {:error, :timeout}
+  end
+
+  defp handle_task_result({:ok, {:error, reason}}) do
+    IO.puts "Error downloading image: #{reason}."
+    {:error, reason}
+  end
+
 
   defp measure_execution_time(fun) do
     {:ok, datetime} = Calendar.DateTime.now("America/Toronto")
@@ -72,7 +161,8 @@ defmodule Crawler do
   end
 
   def run(file_path, output_path) do
-    measure_execution_time(fn -> download_images(file_path, output_path) end)
+    #measure_execution_time(fn -> download_images(file_path, output_path) end)
+    measure_execution_time(fn -> download_images_async_stream(file_path, output_path) end)
   end
 
 end
